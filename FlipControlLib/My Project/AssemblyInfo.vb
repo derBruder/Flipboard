de6dbde16a9a18c61931c587e4ea3c01d1396c75
Daniel Wockel <GIT@wockels.de>
2023-07-13 10:12:26 +0200

    Initial Create UserControl and Testproject

---
+++ FlipControlLib/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die einer Assembly zugeordnet sind.
+
+' Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("FlipControlLib")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("FlipControlLib")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID wird für die typelib-ID verwendet, wenn dieses Projekt für COM verfügbar gemacht wird.
+<Assembly: Guid("c5777895-9b54-4137-b9c0-a848ce7b3241")>
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+' indem Sie "*" wie unten gezeigt eingeben:
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ FlipControlTest/Form1.Designer.vb
@@ -0,0 +1,64 @@
+﻿Imports FlipControlLib
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Flipboardtest
+  Inherits System.Windows.Forms.Form
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()>
+  Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+    Try
+      If disposing AndAlso components IsNot Nothing Then
+        components.Dispose()
+      End If
+    Finally
+      MyBase.Dispose(disposing)
+    End Try
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()>
+  Private Sub InitializeComponent()
+    Me.Flip1 = New FlipControlLib.FlipboardCTL()
+    Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+    Me.Flipboardctl1 = New FlipControlLib.FlipboardCTL()
+    Me.SuspendLayout()
+    '
+    'Flip1
+    '
+    Me.Flip1.Location = New System.Drawing.Point(92, 12)
+    Me.Flip1.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+    Me.Flip1.Name = "Flip1"
+    Me.Flip1.Size = New System.Drawing.Size(139, 248)
+    Me.Flip1.TabIndex = 0
+    '
+    'Flipboardctl1
+    '
+    Me.Flipboardctl1.Location = New System.Drawing.Point(252, 12)
+    Me.Flipboardctl1.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+    Me.Flipboardctl1.Name = "Flipboardctl1"
+    Me.Flipboardctl1.Size = New System.Drawing.Size(139, 248)
+    Me.Flipboardctl1.TabIndex = 1
+    '
+    'Flipboardtest
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(800, 450)
+    Me.Controls.Add(Me.Flipboardctl1)
+    Me.Controls.Add(Me.Flip1)
+    Me.Name = "Flipboardtest"
+    Me.Text = "Flipboardtest"
+    Me.ResumeLayout(False)
+
+  End Sub
+
+  Friend WithEvents Flip1 As FlipboardCTL
+  Friend WithEvents ColorDialog1 As ColorDialog
+  Friend WithEvents Flipboardctl1 As FlipboardCTL
+End Class
